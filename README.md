8a3cc7e08535deb7f984c7de54f8654d0dd09035
Sidimoctar <sidimoctar142@gmail.com>
2021-09-16 18:19:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-e8wz5h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-e8wz5h)
---
+++ index.ts
@@ -0,0 +1,38 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+console.log('bonjour tout le monde');
+let us: number;
+let we: number;
+us = 144;
+we = 22;
+console.log(us * we);
+let pp: number;
+let pp2: number;
+pp = 22;
+pp2 = 33;
+console.log(pp + pp2);
+let pp3: number;
+pp3 = 55;
+let pp4: number;
+pp4 = 44;
+let pp5: number;
+pp5 = 66;
+console.log(pp4 + pp5);
+let pp6: number;
+pp6 = 110;
+let pp7: number;
+pp7 = 77;
+let pp8: number;
+pp8 = 88;
+console.log(pp7 + pp8);
+let pp00: number;
+pp00 = 165;
+console.log(pp6 + pp00);
+let pp60: number;
+let fifa: number;
+fifa = 11;
+let fifa11: number;
+fifa11 = 12;
+console.log(fifa * fifa11);
